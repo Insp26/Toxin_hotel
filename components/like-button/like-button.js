a1f58c2888778f32f3b2b758fd3455543767b01d
Insp26 <42529487+Insp26@users.noreply.github.com>
2020-02-29 08:55:00 +0300

    Updates

--- components/like-button/like-button.js
+++
@@ -1,9 +0,0 @@
-function likeButtonClick() {
-    let likeBtnClick = document.getElementsByClassName("like-button__input")
-    if (likeBtnClick.checked = true) {
-        document.getElementsByClassName("like-button__count").style.content = "+1"
-    }
-    else {
-        document.getElementsByClassName("like-button__count").style.content = "-1"
-    }
-}
