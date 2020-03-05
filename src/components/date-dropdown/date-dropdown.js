f8a6b2a2ed93c96cd8b6ab889e04509c42727637
Insp26 <42529487+Insp26@users.noreply.github.com>
2020-03-05 07:32:36 +0300

    Добавлена возможность узнать кол-во выбранных суток

--- src/components/date-dropdown/date-dropdown.js
+++ src/components/date-dropdown/date-dropdown.js
@@ -1,11 +1,27 @@
+function parseDate(str) {
+    var mdy = str.split('.');
+    return new Date(mdy[2], mdy[1]-1, mdy[0]);
+}
+
+function datediff(first, second) {
+    return Math.round((second-first)/(1000*60*60*24));
+}
+
 let $dateDropdownBtn = $(".date-dropdown__button"),
     $dateDropdownInput = $("#date__start")
     dateDp = $dateDropdownInput.datepicker({
-        onSelect: function (fd, d, picker) { 
-            $("#date__start").val(fd.split("-")[0]);
-            $("#date__end").val(fd.split("-")[1]);
+        onSelect: function onSelect(selectedDates) { 
+            if(selectedDates !== undefined && selectedDates != '' && selectedDates.indexOf('-') > -1){
+                let mdy = selectedDates.split('-');
+                $("#date__start").val(mdy[0]);
+                $("#date__end").val(mdy[1]);
+                let numNights = (datediff(parseDate(mdy[0]), parseDate(mdy[1])));
+                console.log(selectedDates)
+                console.log(numNights)
+            }
         },
         language: 'ru',
+        dateFormat:'dd.mm.yyyy',
         range: true,
         toggleSelected: false,
         minDate: new Date(),
